e81cb98acfea88db148fdb0c6da0a977759d3771
Cody Peck <cspeck7@gmail.com>
2025-12-06 15:25:57 -0600

    fix: revert configuration changes

--- src/main/java/com/speck/mailbox/lib/configuration/MailboxProperties.java
+++ src/main/java/com/speck/mailbox/lib/configuration/MailboxProperties.java
@@ -1,9 +1,32 @@
 package com.speck.mailbox.lib.configuration;
 
+import lombok.Getter;
+
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
-public record MailboxProperties(
-        String table,
-        int batchSize,
-        List<Type> messagesTypes) {}
+@Getter
+public class MailboxProperties {
+
+    private final String table;
+    private final List<Type> messagesTypes;
+    private int batchSize;
+
+    public MailboxProperties(String table) {
+        this.table = table;
+        this.batchSize = 100;
+        this.messagesTypes = new ArrayList<>();
+    }
+
+    public MailboxProperties withBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+        return this;
+    }
+
+    public MailboxProperties withMessageType(Type messageType) {
+        messagesTypes.add(messageType);
+        return this;
+    }
+
+}
--- src/main/java/com/speck/mailbox/lib/configuration/MessageHandlerProperties.java
+++ src/main/java/com/speck/mailbox/lib/configuration/MessageHandlerProperties.java
@@ -1,10 +1,30 @@
 package com.speck.mailbox.lib.configuration;
 
+import lombok.Getter;
+
 import java.lang.reflect.Type;
 
-public record MessageHandlerProperties(
-        Type mailboxMessageHandlerType,
-        Type mailboxMessageType,
-        int boundedCapacity,
-        int maxDegreeOfParallelism) {
+@Getter
+public class MessageHandlerProperties {
+
+    private final Type mailboxMessageHandlerType;
+    private final Type mailboxMessageType;
+    private int boundedCapacity;
+    private int maxDegreeOfParallelism;
+
+    public MessageHandlerProperties(Type mailboxMessageHandlerType, Type mailboxMessageType) {
+        this.mailboxMessageHandlerType = mailboxMessageHandlerType;
+        this.mailboxMessageType = mailboxMessageType;
+    }
+
+    public MessageHandlerProperties withBoundedCapacity(int boundedCapacity) {
+        this.boundedCapacity = boundedCapacity;
+        return this;
+    }
+
+    public MessageHandlerProperties withMaxDegreeOfParallelism(int maxDegreeOfParallelism) {
+        this.maxDegreeOfParallelism = maxDegreeOfParallelism;
+        return this;
+    }
+
 }
